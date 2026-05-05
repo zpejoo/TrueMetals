3731a1f0002c39f845558eef9e84ebf04d5cbe74
zpejoo <ajpcosta29@gmail.com>
2026-05-05 23:46:19 +0000

    Initial Commit

---
+++ src/main/java/com/zpe/truemetals/TrueMetals.java
@@ -0,0 +1,17 @@
+package com.zpe.truemetals;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TrueMetals implements ModInitializer {
+	public static final String MOD_ID = "truemetals";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
